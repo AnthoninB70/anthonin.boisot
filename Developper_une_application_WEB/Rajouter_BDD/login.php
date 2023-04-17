dce63d4c4e95c1f5d0cf13b9b4c9d35460eff99d
AnthoninB70 <109092404+AnthoninB70@users.noreply.github.com>
2023-04-17 14:43:15 +0200

    Create login.php

---
+++ Developper_une_application_WEB/Rajouter_BDD/login.php
@@ -0,0 +1,38 @@
+<?php
+
+if (isset($_POST["login"]) && isset($_POST["password"])) {
+    $bdd = new PDO(
+        "mysql:host=localhost;dbname=anthonin.boisot;charset=utf8",
+        "anthonin.boisot",
+        ""
+    );
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $query = $bdd->prepare(
+        "SELECT * FROM users WHERE login = ? AND password = ?"
+    );
+    $query->execute([$_POST["login"], $_POST["password"]]);
+    $user = $query->fetch();
+
+    if ($user) {
+        if ($user["SuperUser"]) {
+            header("Location: superuser.html");
+        } else {
+            header("Location: user.html");
+        }
+    } else {
+        echo "Login ou mot de passe incorrect.";
+    }
+} else {
+    echo '<form method="post" action="">';
+    echo '<label for="login">Login :</label>';
+    echo '<input type="text" name="login" id="login" />';
+    echo "<br />";
+    echo '<label for="password">Mot de passe :</label>';
+    echo '<input type="password" name="password" id="password" />';
+    echo "<br />";
+    echo '<input type="submit" value="Se connecter" />';
+    echo "</form>";
+}
+
+?>
